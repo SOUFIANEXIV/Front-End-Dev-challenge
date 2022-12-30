4fada62b32f1da49ed596485557f852329cbd5ba
SOUFIANEXIV <98940737+SOUFIANEXIV@users.noreply.github.com>
2022-12-30 17:35:59 +0100

    fix

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ function displayCandidates() {
 
   candidatesToDisplay.forEach((candidate) => {
     allCondidates.innerHTML += `
-    <div class=" bg-transparent flex flex-wrap items-center bg-white  justify-between  pt-6 pl-4 border-b pb-6  ">
+    <div class="  flex flex-wrap items-center bg-white  justify-between  pt-6 pl-4 border-b pb-6  ">
 <div class=" flex gap-4 bg-white ">
 <img src="${candidate.image}" class="h-10 w-10 rounded-full  "  >
 <div>
@@ -188,7 +188,7 @@ function displayPaginationControls() {
   
 
   let paginationControls = `
-    <div class="pagination-controls  flex justify-between mb-5 mt-2">
+    <div class="pagination-controls  px-2 flex justify-between mb-5 mt-2">
       <button class="pagination-button flex   text-gray-500" ${currentPage === currentPage ? 'disabled' : ''}>
     
       <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path></svg>
